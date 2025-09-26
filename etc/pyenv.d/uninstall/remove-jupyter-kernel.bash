8180db07d05ec977e94458d27bae66d8c2c6a346
Chris Munier <cmunier@gmail.com>
2025-09-26 17:48:43 -0500

    added support env variables

--- etc/pyenv.d/uninstall/remove-jupyter-kernel.bash
+++ etc/pyenv.d/uninstall/remove-jupyter-kernel.bash
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+if [[ -z "$JUPYTER_DATA_DIR" ]]; then
+    echo "Jupyter not found. Please install jupyter in one of your pyenv versions." >&2
+    jupyter_root=$(pyenv whence jupyter|tail -1)
+    jupyter=${PYENV_ROOT}/versions/${jupyter_root}/bin/jupyter
+else
+    echo "Using Jupyter data directory: $JUPYTER_DATA_DIR"
+fi
 if declare -Ff after_uninstall >/dev/null; then
-    after_uninstall 'rm -rf $(jupyter --data-dir)/kernels/${VERSION_NAME}'
+    after_uninstall "rm -rf ${JUPYTER_DATA_DIR}/kernels/pyenv_${VERSION_NAME}"
 else
     echo "pyenv: pyenv-jupyter-kernel plugin requires pyenv v0.1.0 or later" >&2
 fi
